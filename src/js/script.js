e52f9e5d9533b870c89cd2153b257f588902fe4b
Antonio <BulletAnton@yandex.ru>
2021-04-18 23:12:02 +0400

    by block project

---
+++ src/js/script.js
@@ -0,0 +1,36 @@
+window.addEventListener('DOMContentLoaded', () => {
+  const menu = document.querySelector('.menu__items'),
+    menuItem = document.querySelectorAll('.menu__item'),
+    hamburger = document.querySelector('.hamburger');
+
+  hamburger.addEventListener('click', () => {
+    hamburger.classList.toggle('hamburger_active');
+    menu.classList.toggle('menu_active');
+  });
+
+  menuItem.forEach((item) => {
+    item.addEventListener('click', () => {
+      hamburger.classList.toggle('hamburger_active');
+      menu.classList.toggle('menu_active');
+    });
+  });
+
+  // video-play
+
+  const play = document.querySelectorAll('.play'),
+    videoWrapper = document.querySelector('.promo__video-wrapper'),
+    wrapper = document.querySelector('.wrapper'),
+    close = document.querySelector('.close');
+
+  play.forEach(val => {
+    val.addEventListener('click', () => {
+        videoWrapper.style.display = 'block';
+        wrapper.innerHTML =
+          '<iframe src="https://www.youtube.com/embed/KvUgaHTNit4?autoplay=1&amp;mute=1&amp;enablejsapi=1&amp;fs=1&amp;modestbranding=1" frameborder="0" allowfullscreen="allowfullscreen" mozallowfullscreen="mozallowfullscreen" msallowfullscreen="msallowfullscreen" oallowfullscreen="oallowfullscreen" webkitallowfullscreen="webkitallowfullscreen"></iframe>';
+      });
+  })
+  
+  close.addEventListener('click', () => {
+    videoWrapper.style.display = 'none';
+  })
+});
